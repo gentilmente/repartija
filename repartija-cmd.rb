8f0f7754bab21a4836e77a084416e0d6eb46e4a7
pestequiel <pestequiel@gmail.com>
2014-11-14 22:19:51 -0300

    corregir indentacion

--- repartija-cmd.rb
+++ repartija-cmd.rb
@@ -1,48 +1,53 @@
 def main 
-	Preparar_listas()
-	Separar_lista()
+    Preparar_listas()
+    Separar_lista()
 
-	$acreedores.each do |a|
-		$acumulado = 0
-		puts "Para acreedor: " + a.to_s
-		$deudores.map! do |d|
-			if(d > 0)
-				puts "el deudor: " + ( $deudores.index( d ) + 1 ).to_s
-				$acumulado += d
-				$resto = $acumulado + a
-				if( $resto > 0 && $resto < $pago_individual)
-					puts "Paga: " + ($pago_individual - $resto).to_s
-					d = $resto
-				elsif ( $resto > $pago_individual)
-					#puts "otro" + $pago_individual.to_s
-					d = $pago_individual
-				else
-					puts "paga: " + $pago_individual.to_s
-					d = 0
-				end
-			end
-		end
-		#$deudores.delete_if { |e| e < 0}
-		puts $deudores.to_s
-	end 
+    $acreedores.each do |a|
+        $acumulado = 0
+        puts "Para acreedor: " + a.to_s
+        $deudores.map! do |d|
+            if(d > 0)
+                puts "el deudor: " + ( $deudores.index( d ) + 1 ).to_s
+                $acumulado += d
+                puts $resto = $acumulado + a
+                if( $resto > 0 && $resto < $pago_individual)
+                    puts "Paga: " + ($pago_individual - $resto).to_s
+                    d = $resto
+                elsif ( $resto > $pago_individual)
+                    puts "No paga"
+                    d = $pago_individual
+                elsif (d < $pago_individual)
+                    puts "ppaga: " + d.to_s
+                    d = 0
+                else
+                    puts "paga: " + $pago_individual.to_s
+                    d = 0
+                end
+            else
+                d = 0
+            end
+        end
+        #$deudores.delete_if { |e| e < 0}
+        puts $deudores.to_s
+    end 
 end
 
 def Preparar_listas()
-	$aportes = [73,27,0,0,0,0,0,0,0,0]
-	puts $aportes.to_s
-	puts "Total: " 
-	puts $total = $aportes.reduce(:+)
-	puts "Pago individual: " 
-	puts $pago_individual = $total/$aportes.count
-	$saldos = $aportes.map {|e| $pago_individual - e }
+    $aportes = [43,27,30,0,0,0,0,0,0,0]
+    puts $aportes.to_s
+    puts "Total: " 
+    puts $total = $aportes.reduce(:+)
+    puts "Pago individual: " 
+    puts $pago_individual = $total/$aportes.count
+    $saldos = $aportes.map {|e| $pago_individual - e }
 end
 
 def Separar_lista()
-	$acreedores, $deudores = $saldos.partition { |e| e < 0 }
-	puts "acreedores: "
-	puts $acreedores.to_s
-	puts "deudores: "
-	puts $deudores.to_s	
+    $acreedores, $deudores = $saldos.partition { |e| e < 0 }
+    puts "acreedores: "
+    puts $acreedores.to_s
+    puts "deudores: "
+    puts $deudores.to_s 
 end
 
 main()
