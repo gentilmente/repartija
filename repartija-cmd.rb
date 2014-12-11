83bf04355303d93325dc1b8236e7664070c24ac5
pestequiel <pestequiel@gmail.com>
2014-12-11 07:26:44 -0300

    refactor consola

--- repartija-cmd.rb
+++ repartija-cmd.rb
@@ -11,44 +11,51 @@ end
 
 def Separar_lista()
     $acreedores, $deudores = $saldos.partition { |e| e < 0 }
+    puts '- -- - -- - -- -'
     puts "acreedores: "
     puts $acreedores.to_s
     puts "deudores: "
     puts $deudores.to_s 
+    puts '- -- - -- - -- -'
 end
 
 def Calcular()
-    $acreedores.each do |a|
+    $acreedores.map! do |a|
         $acumulado = 0
         puts
         puts "Para acreedor: " + a.to_s
         $deudores.map! do |d|
-            if(d > 0)
-                puts
+            if(d > 0 && a < 0)
                 puts "el deudor: " + ( $deudores.index( d ) + 1 ).to_s
-                $acumulado += d
-                $resto = $acumulado + a
-                if( $resto > 0 && $resto < $pago_individual)
-                    puts "Paga: " + ($pago_individual - $resto).to_s
-                    d = $resto
+                $acumulado += $pago_individual
+                $resta_pagar = $acumulado + a
+                if( $resta_pagar > 0 && $resta_pagar < $pago_individual)
+                    puts "Paga: " + ($pago_individual - $resta_pagar).to_s
+                    a += $pago_individual - $resta_pagar
+                    d = $resta_pagar
                 elsif (d < $pago_individual)
                     puts "ppaga: " + d.to_s
+                    a += d
                     d = 0
-                elsif ( $resto > $pago_individual)
+                elsif ( $resta_pagar > $pago_individual)
                     puts "No paga"
                     d = $pago_individual
                 else
                     puts "paga: " + $pago_individual.to_s
                     d = 0
+                    a += $pago_individual
                 end
-                #puts $resto
+                #puts $resta_pagar
             else
                 d = 0
             end
-            #puts $resto
+            #puts $resta_pagar
         end
         #$deudores.delete_if { |e| e < 0}
+        puts '- -- - -- - -- -'
+        puts $acreedores.to_s
         puts $deudores.to_s
+        puts '- -- - -- - -- -'
     end 
 end
 
