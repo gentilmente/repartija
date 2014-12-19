17f0835f26565f0c813ed61166501a62ba151aef
pestequiel <peste@mbp.local>
2014-12-18 22:19:15 -0300

    gemfile para debug y rbenv + algoritmo sin map!

--- repartija-cmd.rb
+++ repartija-cmd.rb
@@ -1,6 +1,6 @@
 
 def Preparar_listas()
-    $aportes = [123,123,0,0,0,0]
+    $aportes = [52,65,0,0,44]
     puts $aportes.to_s
     puts "Total: " 
     puts $total = $aportes.reduce(:+)
@@ -20,42 +20,40 @@ def Separar_lista()
 end
 
 def Calcular()
-    $acreedores.map! do |a|
+    $acreedores.each do |a|
         $acumulado = 0
         puts
         puts "Para acreedor: " + a.to_s
-        $deudores.map! do |d|
+        $deudores.each do |d|
             if(d > 0 && a < 0)
                 puts "el deudor: " + ( $deudores.index( d ) + 1 ).to_s
-                $acumulado += $pago_individual
-                $resta_pagar = $acumulado + a
+                puts $acumulado += $pago_individual
+                puts $resta_pagar = $acumulado + a
                 if( $resta_pagar > 0 && $resta_pagar < $pago_individual)
                     puts "Paga: " + ($pago_individual - $resta_pagar).to_s
-                    a += $pago_individual - $resta_pagar
-                    d = $resta_pagar
+                    $acreedores[$acreedores.index(a)] += $pago_individual - $resta_pagar
+                    $deudores[$deudores.index(d)] = $resta_pagar
                 elsif (d < $pago_individual)
                     puts "ppaga: " + d.to_s
-                    a += d
-                    d = 0
+                    $acreedores[$acreedores.index(a)] += d
+                    $deudores[$deudores.index(d)] =  0
                 elsif ( $resta_pagar > $pago_individual)
                     puts "No paga"
-                    d = $pago_individual
+                    $deudores[$deudores.index(d)] =  $pago_individual
                 else
                     puts "paga: " + $pago_individual.to_s
-                    d = 0
-                    a += $pago_individual
+                    $deudores[$deudores.index(d)] =  0
+                    $acreedores[$acreedores.index(a)] += $pago_individual
                 end
                 #puts $resta_pagar
             else
-                d = 0
+                $deudores[$deudores.index(d)] =  0
             end
-            #puts $resta_pagar
+            puts '- -- - -- - -- -'
+            puts $acreedores.to_s
+            puts $deudores.to_s
+            puts '- -- - -- - -- -'
         end
-        #$deudores.delete_if { |e| e < 0}
-        puts '- -- - -- - -- -'
-        puts $acreedores.to_s
-        puts $deudores.to_s
-        puts '- -- - -- - -- -'
     end 
 end
 
