c82ea50efe8e71b052ff5c9baebeb53800c3639d
pestequiel <peste@mbp.local>
2014-12-30 10:26:41 -0300

    generate resultados hash for result view
    + refactor + HashToHTML

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require 'sinatra'
 
 class Hash
   def to_html
-    ['<ul class="izq">',map { |k, v| ["<li><strong>#{k}:</strong>", v.respond_to?(:to_html) ?
+    ['<ul>',map { |k, v| ["<li><strong>#{k}:</strong>", v.respond_to?(:to_html) ?
      v.to_html : "<span> #{v}</span></li>"] },'</ul>'].join
   end
 end
@@ -30,13 +30,13 @@ helpers do
 
   def hard_code_aportes()
     settings.aportes = { 
-      "Bufarra" => 40, 
-      "Martin" => 600,  
-      "Joni" => 150,  
-      "Pedro" => 0,  
-      "Cachi" => 0, 
-      "Gisela" => 200,
-      "Eze" => 0  
+      Bufarra: 40, 
+      Martin: 600,  
+      Joni: 150,  
+      Pedro: 0,  
+      Cachi: 0, 
+      Gisela: 200,
+      Eze: 0  
     }
   end
 
@@ -65,7 +65,6 @@ helpers do
       @acumulado = 0
       puts'-------------------------------------------------------------------------'
       puts "Para acreedor: " + nombre_acr.to_s + monto_acr.to_s
-      #settings.resultados <= nombre_acr.to_sym
       settings.deudores.each  do |k, v| 
         #puts "monto acreedor: " + monto_acr.to_s
         #puts
@@ -75,23 +74,38 @@ helpers do
           @resta_pagar = @acumulado + monto_acr
           #puts "acumulado: " + @acumulado.to_s
           #puts "resta_pagar: " + @resta_pagar.to_s
-
           if( @resta_pagar > 0 && @resta_pagar < @pago_individual)
             puts "Paga: " + (@pago_individual - @resta_pagar).to_s
             settings.deudores[k] = @resta_pagar
             settings.acreedores[nombre_acr] += @pago_individual - @resta_pagar
             @monto_acr_actual = settings.acreedores[nombre_acr]
+            if(!settings.resultados.has_key?(nombre_acr))
+              settings.resultados[nombre_acr.to_sym] = {k.to_sym => @pago_individual - @resta_pagar}
+            else
+              settings.resultados[nombre_acr].store(k, @pago_individual - @resta_pagar)
+            end
+
           elsif (v < @pago_individual)
             puts "ppaga: " + v.to_s
             settings.deudores[k] = 0
             settings.acreedores[nombre_acr] += v
             @monto_acr_actual = settings.acreedores[nombre_acr]
+            if(!settings.resultados.has_key?(nombre_acr))
+              settings.resultados[nombre_acr.to_sym] = {k.to_sym => v}
+            else
+              settings.resultados[nombre_acr].store(k, v)
+            end
 
           elsif (@resta_pagar <= 0)
             puts "paga: " + @pago_individual.to_s
             settings.deudores[k] = 0
             settings.acreedores[nombre_acr] += @pago_individual
             @monto_acr_actual = settings.acreedores[nombre_acr]
+            if(!settings.resultados.has_key?(nombre_acr))
+              settings.resultados[nombre_acr.to_sym] = {k.to_sym => @pago_individual}
+            else
+              settings.resultados[nombre_acr].store(k, @pago_individual)
+            end
 
           end
         end
@@ -100,6 +114,21 @@ helpers do
       end 
     end
     puts'-------------------------------------------------------------------------' 
+  end
+
+  def HashToHTML(hash, opts = {})
+    return if !hash.is_a?(Hash)
+    indent_level = opts.fetch(:indent_level) { 0 }
+    out = " " * indent_level + "<ul>\n"
+    hash.each do |key, value|
+      out += " " * (indent_level + 2) + "<li><strong>#{key}:</strong>"
+      if value.is_a?(Hash)
+        out += "\n" + HashToHTML(value, :indent_level => indent_level + 2) + " " * (indent_level + 2) + "</li>\n"
+      else
+        out += " <span>#{value}</span></li>\n"
+      end
+    end
+    out += " " * indent_level + "</ul>\n"
   end
 end
 
@@ -124,6 +153,9 @@ post '/' do
     separar_lista()
     calcular()
     @saldos = settings.saldos
+    puts settings.resultados
+    puts settings.resultados.to_html
+    puts HashToHTML(settings.resultados)
     erb :result
   else
     erb :form
