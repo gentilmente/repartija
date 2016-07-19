ef3a7bcd55888293aa52a027ed0c48c53aa178ea
Sebastian Rabuini <sebas@wasabit.com.ar>
2016-07-19 16:06:02 -0300

    Fix styles

--- main.rb
+++ main.rb
@@ -4,27 +4,33 @@ require 'sinatra'
 
 class Hash
   def to_html
-    ['<ul>',map { |k, v| ["<li>#{k}: ", v.respond_to?(:to_html) ?
-     v.to_html : "<span>$ #{v}</span></li>"] },'</ul>'].join
+    list_items = map do |k, v|
+      html_value = v.respond_to?(:to_html) ? v.to_html : "<span>$ #{v}</span>"
+      ["<li>#{k}: ", html_value, '</li>']
+    end
+
+    ['<ul>', list_items, '</ul>'].join
   end
 end
 
 configure do
   enable :sessions
-  set :session_secret, "My session secret"
-  set :bind, '0.0.0.0' #para acceder en WLAN a la IP del server http://192.168.0.10:4567
+  set :session_secret, 'My session secret'
+
+  # para acceder en WLAN a la IP del server http://192.168.0.10:4567
+  set :bind, '0.0.0.0'
 end
 
 helpers do
   def title
-    @title || "Repartija"
+    @title || 'Repartija'
   end
 
   def set_input(user_name, paiment)
     session[:input][user_name.to_sym] ||= paiment
   end
 
-  def hard_code_input()
+  def hard_code_input
     session[:input] = {
       Bufarra: 40,
       Martin: 378,
@@ -38,22 +44,24 @@ helpers do
 
   def prepare_data_set(input)
     @total = input.values.reduce(:+)
-    @individual_paiment = @total/input.length
-    return input.inject({}) do |hash, (k, v)|
-     hash.merge( k.to_sym => @individual_paiment - v )
-   end
+    @individual_paiment = @total / input.length
+
+    input.inject({}) do |hash, (k, v)|
+      hash.merge(k.to_sym => @individual_paiment - v)
+    end
   end
 
   def devide_list(balance)
-    creditors, debtors = balance.partition { |_,e| e < 0 }
+    creditors, debtors = balance.partition { |_, e| e < 0 }
     creditors = creditors.to_h
     debtors = debtors.to_h
+
     return creditors, debtors
   end
 
   def generate_output(creditor, debtor, paiment)
-    if(!@result.key?(creditor))
-      @result[creditor.to_sym] = {debtor.to_sym => paiment}
+    if !@result.key?(creditor)
+      @result[creditor.to_sym] = { debtor.to_sym => paiment }
     else
       @result[creditor].store(debtor, paiment)
     end
@@ -67,15 +75,15 @@ helpers do
   end
 
   def pay(debtor, debt, creditor, creditor_amount)
-    if(debt > 0 && @actual_creditor_amount < 0)
+    if debt > 0 && @actual_creditor_amount < 0
       @creditor_accum += debt
       @yet_to_pay = @creditor_accum + creditor_amount
-      if( @yet_to_pay > 0 && @yet_to_pay < @individual_paiment)
+      if @yet_to_pay > 0 && @yet_to_pay < @individual_paiment
         paiment = debt - @yet_to_pay
         balance(creditor, debtor, paiment)
-      elsif (debt < @individual_paiment)
+      elsif debt < @individual_paiment
         balance(creditor, debtor, debt)
-      elsif (@yet_to_pay <= 0)
+      elsif @yet_to_pay <= 0
         balance(creditor, debtor, @individual_paiment)
       end
     end
@@ -84,8 +92,9 @@ helpers do
   def collect(creditor, creditor_amount)
     @actual_creditor_amount = creditor_amount
     @creditor_accum = 0
-    @debtors.each  do |debtor, debt|
-      pay(debtor, debt,creditor, creditor_amount)
+
+    @debtors.each do |debtor, debt|
+      pay(debtor, debt, creditor, creditor_amount)
     end
   end
 
@@ -96,23 +105,24 @@ helpers do
     @creditors.each do |creditor, creditor_amount|
       collect(creditor, creditor_amount)
     end
-    return @result
+
+    @result
   end
 
-  def HashToHTML(hash, opts = {})
-    return if !hash.is_a?(Hash)
+  def hash_to_html(hash, opts = {})
+    return unless hash.is_a?(Hash)
     indent_level = opts.fetch(:indent_level) { 0 }
-    out = " " * indent_level + "<ul>\n"
+    out = ' ' * indent_level + "<ul>\n"
     hash.each do |key, value|
-      out += " " * (indent_level + 2) + "<li><strong>#{key}:</strong>"
+      out += ' ' * (indent_level + 2) + "<li><strong>#{key}:</strong>"
       if value.is_a?(Hash)
-        out += "\n" + HashToHTML(value, :indent_level => indent_level + 2)
-        + " " * (indent_level + 2) + "</li>\n"
+        out += "\n" + hash_to_html(value, indent_level: indent_level + 2)
+        + ' ' * (indent_level + 2) + "</li>\n"
       else
         out += " <span>#{value}</span></li>\n"
       end
     end
-    out += " " * indent_level + "</ul>\n"
+    out += ' ' * indent_level + "</ul>\n"
   end
 end
 
@@ -121,18 +131,18 @@ get '/' do
 end
 
 get '/form' do
-  session[:input] ||= Hash.new
+  session[:input] ||= {}
   erb :form
 end
 
 post '/form' do
-  @title = "Resultado"
+  @title = 'Resultado'
   @user_name = params[:user_name].chomp
 
   #====================== Para testing =================================
 
   set_input(@user_name, params[:cantidad].to_i)
-  #hard_code_input()
+  # hard_code_input()
 
   #=====================================================================
 
